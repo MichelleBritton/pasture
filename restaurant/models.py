d9e0a0e64e02fa7826e912a08f05d893d76e7cdd
Michelle Britton <michelle@daisy-webdesign.co.uk>
2023-11-24 16:39:11 +0000

    Add booking model and add model to admin panel

--- restaurant/models.py
+++ restaurant/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Booking(models.Model):
+    username = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="booking"
+    )
+    phone_no = models.CharField(max_length=11, default=0)
+    date = models.DateField()
+    time = models.IntegerField()
+    no_of_guests = models.IntegerField()
+    notes = models.TextField()
+
+

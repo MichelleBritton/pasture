7a6024b597861b1efa4ad704940253630b91c769
Michelle Britton <michelle@daisy-webdesign.co.uk>
2023-12-07 15:02:52 +0000

    Add Menu Form

--- restaurant/forms.py
+++ restaurant/forms.py
@@ -22,4 +22,13 @@ class BookTableForm(forms.ModelForm):
             'date': DateInput(),
         }
     
-    
+
+class MenuForm(forms.ModelForm):
+    """ 
+    Create a form based on the Menu model so that
+    staff can create a menu
+    """
+
+
+    class Meta:
+        model = Menu

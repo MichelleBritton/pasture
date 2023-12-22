fe867764232ac437e4242f0dac06129826f875b1
Michelle Britton <michelle@daisy-webdesign.co.uk>
2023-12-22 13:06:05 +0000

    Add clean function to stop bookings being made in the past

--- restaurant/forms.py
+++ restaurant/forms.py
@@ -1,6 +1,8 @@
 from .models import Booking
 from .models import Menu
 from django import forms
+from datetime import datetime
+from django.core.exceptions import ValidationError
 
 
 class DateInput(forms.DateInput):
@@ -23,6 +25,19 @@ class BookTableForm(forms.ModelForm):
         widgets = {
             'date': DateInput(),
         }
+
+    def clean(self):
+        """
+        Get form data and clean to ensure reservations cannot be 
+        made in the past
+        """
+        date = self.cleaned_data['date']
+
+        # Throw validation errors if date is in the past
+        if date < datetime.today().date():
+            raise ValidationError(
+                'Invalid date - Reservation cannot be made in the past'
+            )
 
 
 class MenuForm(forms.ModelForm):
